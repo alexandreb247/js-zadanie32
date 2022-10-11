46d5e34ba5098e4bc64015ead4743155980f8fa0
alexandreb247 <alexandre@poczta.onet.pl>
2022-10-11 08:58:39 +0200

    zadanie wykonane

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let num1 = 2;
+let num2 = 5;
+
+function porownanie(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+  }
+console.log(porownanie(num1, num2));
